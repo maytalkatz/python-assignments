9d401800af0ef08d2ea18daf3860afc6eddc88c9
maytalkatz <maytalkatz@gmail.com>
2024-11-17 22:36:48 +0200

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,7 @@
+import math
+
+radius = 23
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+print ("The area of the circle is", area)
+print("The circumference of the circle is", circumference)
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+width = 14
+length = 7
+area = width * length
+circumference = 2 * (width + length)
+print("The area of the rectangle is", area)
+print("The circumference of the rectangle is", circumference)
